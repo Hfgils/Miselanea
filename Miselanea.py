565f81e7665b8a02039b69d8d143e215f2aaef3b
Herney <genkuro@misena.educ.o>
2022-03-30 09:26:53 -0500

    inicio

---
+++ Miselanea.py
@@ -0,0 +1,155 @@
+import math
+import string
+
+
+class Miselanea:
+    a: float
+    conv: float
+
+    @staticmethod
+    def area_triangulo(b, h):
+        a = (b * h) / 2
+
+        return print("El area del trinagulo de base {} y altura {} es: {}".format(b, h, a))
+
+    @staticmethod
+    def sumaAB(x, y):
+        suma = x + y
+        return print("La suma de {} y {} es: {}".format(x, y, suma))
+
+    @staticmethod
+    def potencia(x):
+        p = (x ** 2)
+        return print('El cuadrado de {} es: {}'.format(x, p))
+
+    @staticmethod
+    def convercion(us) -> object:
+        conv = format((us * 1.10), '.2f')
+        return print('Por {} US recivira {} EUR '.format(us, conv))
+
+    @staticmethod
+    def apcuadrado(lado) -> object:
+        ar = lado ** 2
+        per = lado * 4
+        return print(
+            'El area del cuadrado de lado ' + lado + 'su area es: ' + str(ar) + ' y su perimetro es' + str(per))
+
+    @staticmethod
+    def avcilindro(r, h) -> object:
+        ar = round(2 * math.pi * r * (h + r), 2)
+        vol = round(math.pi * (r ** 2) * h, 2)
+        return print('Para un cilindro de altura ' + str(h) + ' y radio ' + str(r) + ' su area es: ' + str(
+            ar) + ' y su volumen es: ' + str(vol))
+
+    @staticmethod
+    def alcircunferencia(r) -> object:
+        ar = round((math.pi * (r ** 2)), 2)
+        lon = round(2 * math.pi * r, 2)
+
+        insert_var = "Para una circunferencia de radio  {} su area es: {} y su longitud es: {}".format(r, ar, lon)
+        return print(insert_var)
+
+    @staticmethod
+    def promedio(x, y, z) -> object:
+        prom = round((x + y + z) / 3, 2)
+
+        insert_var = "El promedio de los numeros {},{} y {} es: {}".format(x, y, z, prom)
+
+        # print
+        print(insert_var)
+
+    @staticmethod
+    def negativopositivo(x) -> object:
+        if x > 0:
+            print("El número {} es positivo.".format(x))
+        elif x < 0:
+            print("El número {} negativo".format(x))
+
+    @staticmethod
+    def mayormenor2(x, y) -> object:
+        mayor = x
+        if mayor > y:
+            menor = y
+        elif mayor < y:
+            menor = mayor
+            mayor = y
+
+        print("El número {} es mayor y el número {} es menor.".format(mayor, menor))
+
+    @staticmethod
+    def mayormenor3(x, y, z) -> object:
+        mayor = x
+        if mayor > y:
+            menor = y
+        elif mayor < y:
+            menor = mayor
+            mayor = y
+
+        print("El número {} es mayor y el número {} es menor.".format(mayor, menor))
+
+    @staticmethod
+    def suma2(a, b) -> object:
+        resultado:int
+        mensaje:string
+
+        if a < b:
+            resultado=a+b
+            mensaje= (" la operacion {} + {} es igual a {}.".format(a, b,resultado))
+        elif a>b:
+            resultado= a-b
+            mensaje= (" la operacion {} - {} es igual a {}.".format(a, b,resultado))
+
+        return print(mensaje)
+
+    @staticmethod
+    def suma3(a, b) -> object:
+        resultado: int
+        mensaje: string
+
+        if a < 0 or b<0:
+            resultado = a + b
+            mensaje = (" la operacion {} + {} es igual a {}.".format(a, b, resultado))
+        else:
+            resultado = a * b
+            mensaje = (" la operacion {} * {} es igual a {}.".format(a, b, resultado))
+
+        return print(mensaje)
+
+
+    @staticmethod
+    def multipos3() -> object:
+        j: int
+        j=3
+
+        for x in range(100):
+            x += 1
+
+            if (x%3==0):
+                print(str(x))
+
+    @staticmethod
+    def impares() -> object:
+        j: int
+        j=3
+
+        for x in range(100):
+
+
+            if (x%2!=0):
+                print(str(x))
+
+    @staticmethod
+    def cuadrados30() -> object:
+        j: int
+
+
+        for x in range(5):
+            x += 1
+            j=(x**2)
+            print(str(j))
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,405 @@
+import os
+import string
+
+import Miselanea
+
+mise = Miselanea.Miselanea
+var: string
+menup = """
+Bienvenidos a la micelanea de Pyton,a continuación selecione 
+la opción de su prefiera:
+
+[1] Operadores 
+[2] Condicionales
+[3] Ciclos 
+[3] Salir
+"""
+
+menuo = """
+Bienvenidos a la micelanea de Pyton,a continuación selecione 
+la opción de su prefiera:
+
+[1] área de un triángulo.
+[2] Suma de dos números.
+[3] calcular el cuadrado de un número.
+[4] calculo de euros a dólares.
+[5] Calculo del área y del perímetro de un cuadrado.
+[6] Calcular el área y el volúmen de un cilindro.
+[7] Calcular longitud el área de un circulo.
+[8] Calcular el promedio de tres (3) números .
+[99] Salir.
+"""
+
+menuc = """
+Bienvenidos a la micelanea de Pyton,a continuación selecione 
+la opción de su prefiera:
+
+[1] saber si el número ingresado por teclado es positivo o negativo.
+[2] Evaluar  dos números y diga cuál es el mayor y cuál el menor.
+[3] Ingrese tres números enteros positivos y que calcule e imprima en
+    pantalla el menor y el mayor de ellos.
+[4] Dados dos números A y B, sumarlos si A es menor que B o sino restarlos.
+[5] Calculo del área y del perímetro de un cuadrado.
+[6] Calcular el área y el volúmen de un cilindro.
+[7] Ingrese dos números , sumarlos si al menos uno de ellos es negativo, en caso contrario
+    multiplicarlos.
+[8] Calcular el promedio de tres (3) números .
+[99] Salir.
+"""
+
+menuci = """
+Bienvenidos a la micelanea de Pyton,a continuación selecione 
+la opción de su prefiera:
+[1] Operadores 
+[2] Condicionales
+[3] Ciclos 
+"""
+
+
+def operadores() -> object:
+    os.system('cls')
+    print(menuo)
+    opcion = int(input("Introduce la opcion: "))
+
+    while True:
+        if opcion == 1:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                b = int(input("Introduce la base del triangulo: "))
+                h = int(input("Introduce la altura del trangulo: "))
+                mise.area_triangulo(b, h)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO)"))
+                if op == 1:
+                    flag = 1
+            operadores()
+
+        elif opcion == 2:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                b = int(input("Introduce el primer número: "))
+                h = int(input("Introduce el segundo número: "))
+                mise.sumaAB(b, h)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            operadores()
+        elif opcion == 3:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                b = int(input("Introduce el número que quiere elevar al cuadrado: "))
+                mise.potencia(b)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            operadores()
+        elif opcion == 4:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                b = int(input("Ingrese los la cantidad de Dolares que desea cambiar: "))
+                mise.convercion(b)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            operadores()
+        elif opcion == 5:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                r = int(input("Ingrese el valor del lado del cuadrado: "))
+                mise.apcuadrado(r)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            operadores()
+
+        elif opcion == 6:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                r = int(input("Ingrese el radio del cilindro: "))
+                h = int(input("Ingrese la altura del cilindor: "))
+                mise.avcilindro(r, h)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            operadores()
+        elif opcion == 7:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                r = int(input("Ingrese el radio del circunferencia: "))
+                mise.alcircunferencia(r)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            operadores()
+        elif opcion == 8:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                x = int(input("Ingrese el primer número: "))
+                y = int(input("Ingrese el segundo número: "))
+                z = int(input("Ingrese el tercer número: "))
+                mise.promedio()
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            operadores()
+        elif opcion == 99:
+            break
+        else:
+            print('Por favor digite una opcion correcta')
+            os.system("cls")
+            pmenu(var)
+
+
+def condicionales():
+    os.system("cls")
+    print(menuc)
+    opcion = int(input("Introduce la opcion: "))
+
+    while True:
+        if opcion == 1:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                b = int(input("Introduce el número q desea evaluer: "))
+                mise.negativopositivo(x)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO)"))
+                if op == 1:
+                    flag = 1
+            operadores()
+
+        elif opcion == 2:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                b = int(input("Introduce el primer número: "))
+                h = int(input("Introduce el segundo número: "))
+                mise.mayormenor2(b, h)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            condicionales()
+        elif opcion == 3:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                b = int(input("Introduce el número que quiere elevar al cuadrado: "))
+                mise.potencia(b)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            condicionales()
+        elif opcion == 4:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                a = int(input("Ingrese el primer número: "))
+                b = int(input("Ingrese el segundo múmero: "))
+                mise.suma2(a,b)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            condicionales()
+        elif opcion == 5:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                r = int(input("Ingrese el valor del lado del cuadrado: "))
+                mise.apcuadrado(r)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            condicionales()
+
+        elif opcion == 6:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                a = int(input("Ingrese el primer número: "))
+                b = int(input("Ingrese el segundo múmero: "))
+                mise.suma3(a, b)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            condicionales()
+        elif opcion == 7:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                r = int(input("Ingrese el radio del circunferencia: "))
+                mise.alcircunferencia(r)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            condicionales()
+        elif opcion == 8:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                x = int(input("Ingrese el primer número: "))
+                y = int(input("Ingrese el segundo número: "))
+                z = int(input("Ingrese el tercer número: "))
+                mise.promedio()
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            condicionales()
+        elif opcion == 99:
+            break
+        else:
+            print('Por favor digite una opcion correcta')
+            os.system("cls")
+            pmenu(var)
+
+def ciclos():
+    os.system("cls")
+    print(menuc)
+    opcion = int(input("Introduce la opcion: "))
+
+    while True:
+        if opcion == 1:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                mise.multipos3()
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO)"))
+                if op == 1:
+                    flag = 1
+            ciclos()
+
+        elif opcion == 2:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                mise.impares()
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO)"))
+                if op == 1:
+                    flag = 1
+            ciclos()
+        elif opcion == 3:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                mise.pares()
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO)"))
+                if op == 1:
+                    flag = 1
+            ciclos()
+        elif opcion == 4:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                mise.cuadrados30()
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO)"))
+                if op == 1:
+                    flag = 1
+            ciclos()
+        elif opcion == 5:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                r = int(input("Ingrese el valor del lado del cuadrado: "))
+                mise.apcuadrado(r)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            condicionales()
+
+        elif opcion == 6:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                a = int(input("Ingrese el primer número: "))
+                b = int(input("Ingrese el segundo múmero: "))
+                mise.suma3(a, b)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            condicionales()
+        elif opcion == 7:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                r = int(input("Ingrese el radio del circunferencia: "))
+                mise.alcircunferencia(r)
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            condicionales()
+        elif opcion == 8:
+            flag: int = 2
+            op: int
+            while flag == 2:
+                os.system('cls')
+                x = int(input("Ingrese el primer número: "))
+                y = int(input("Ingrese el segundo número: "))
+                z = int(input("Ingrese el tercer número: "))
+                mise.promedio()
+                op = int(input("Desea regresar al menu anterior 1(SI) 2(NO): "))
+                if op == 1:
+                    flag = 1
+            condicionales()
+        elif opcion == 99:
+            break
+        else:
+            print('Por favor digite una opcion correcta')
+            os.system("cls")
+            pmenu(var)
+
+
+
+def pmenu(var):
+    os.system('cls')
+    print(menup)
+    opcion = int(input("Introduce la opcion: "))
+
+    while True:
+        if opcion == 1:
+            operadores()
+        elif opcion == 2:
+            condicionales()
+        elif opcion == 3:
+            ciclos()
+        elif opcion == 99:
+            break
+        else:
+            print('Por favor digite una opcion correcta')
+            os.system("cls")
+            pmenu(var)
+
+
+if __name__ == '__main__':
+    if os.name == "posix":
+        var = "clear"
+    elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+        var = "cls"
+    pmenu(var)
